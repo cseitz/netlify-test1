28dc4b0f9f923e2a3af86e75e829d6542c5bfb50
cseitz5 <cseitz5@kent.edu>
2020-01-22 23:29:11 -0500

    yeet

--- express/index.js
+++ express/index.js
@@ -2,6 +2,11 @@ var express = require("express");
 var _app = express();
 var app = express();
 
+_app.use((req, res, next) => {
+  console.log(req.protocol + '://' + req.get('host') + req.originalUrl);
+  next();
+})
+
 _app.use('/.netlify/functions/index', app);
 
 app.get("/", (req, res) => {
