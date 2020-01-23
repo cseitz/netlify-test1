ee06cf3d90a46d600cf92689be9907e967eef490
cseitz5 <cseitz5@kent.edu>
2020-01-22 23:39:13 -0500

    ok

--- express/server.js
+++ express/server.js
@@ -1,5 +1,14 @@
 var express = require("express");
 var app = express();
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("yeah boi");
+})
+
+router.get("/omg", (req, res) => {
+  res.send("omg what");
+})
 
 app.use("/.netlify/functions/server", require("./paths.js"));
 
