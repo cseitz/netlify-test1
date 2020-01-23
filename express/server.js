7c00ec0648e6d546325585f973a5d70907e2ae22
cseitz5 <cseitz5@kent.edu>
2020-01-22 23:55:22 -0500

    ok

--- express/server.js
+++ express/server.js
@@ -16,8 +16,15 @@ router.get("/omg", (req, res) => {
 
 app.use("/.netlify/functions/server", router);
 
-app.use("/", (req, res) => {
-  res.sendFile(__dirname + "/../index.html");
+app.use((req, res) => {
+  //res.sendFile(__dirname + "/../index.html");
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`<!DOCTYPE html>
+  <object
+    type="text/html"
+    data="/.netlify/functions/server${req.originalUrl}"
+    style="width:100%;height:100%;"
+  >`);
 })
 
 module.exports = app;
