e64401c4a9a41fed1dec604e4033ada8cc2b4f12
cseitz5 <cseitz5@kent.edu>
2020-01-22 23:58:57 -0500

    pls

--- express/server.js
+++ express/server.js
@@ -17,15 +17,19 @@ router.get("/omg", (req, res) => {
 app.use("/.netlify/functions/server", router);
 
 app.use((req, res) => {
+  res.sendFile(__dirname + "/../index.html");
+})
+
+/*app.use((req, res) => {
   //res.sendFile(__dirname + "/../index.html");
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<!DOCTYPE html>
+  res.write(`<!DOCTYPE html><script> alert("hi"); </script>
   <object
     type="text/html"
     data="/.netlify/functions/server${req.originalUrl}"
     style="width:100%;height:100%;"
   >`);
-})
+})*/
 
 module.exports = app;
 module.exports.handler = require('serverless-http')(app);
